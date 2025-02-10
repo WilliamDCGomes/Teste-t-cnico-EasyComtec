397e7d9c1151d3fb2d0c62a1f642530d57fc83ec
William Douglas <williamdouglasgomes@hotmail.com>
2025-02-09 21:36:38 -0300

    Adicionando resolução

---
+++ Teste Técnico.cs
@@ -0,0 +1,40 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        Console.WriteLine(DetectCapitalUse("UsA"));
+    }
+
+    static bool DetectCapitalUse(string word)
+    {
+        if (char.IsUpper(word[0]))
+        {
+            if (word.Length > 1)
+            {
+                if (char.IsUpper(word[1]))
+                {
+                    for (int letter = 1; letter < word.Length; letter++)
+                    {
+                        if (!char.IsUpper(word[letter])) return false;
+                    }
+                }
+                else
+                {
+                    for (int letter = 1; letter < word.Length; letter++)
+                    {
+                        if (char.IsUpper(word[letter])) return false;
+                    }
+                }
+            }
+            return true;
+        }
+        else
+        {
+            for (int letter = 0; letter < word.Length; letter++)
+            {
+                if (char.IsUpper(word[letter])) return false;
+            }
+            return true;
+        }
+    }
+}
